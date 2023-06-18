a6939879ac589da256f05dc9df16b6348c0000ad
GDana <danmi2857@naver.com>
2023-06-19 00:28:46 +0900

    chapter3-1, 3-2

--- src/com/algo/java/hskim/greedy/book/Book01.java
+++ src/com/algo/java/hskim/greedy/book/Book01.java
@@ -1,4 +1,20 @@
 package com.algo.java.hskim.greedy.book;
 
 public class Book01 {
+    /**
+     * 거스름돈(p87)
+     */
+    public static void main(String[] args) {
+        int count = 0;
+        int[] coinTypes = {500,100,50,10};
+        int n = 1260;
+
+        for(int i = 0; i < coinTypes.length; i++) {
+            int coin = coinTypes[i];
+            count += (n / coin);
+            n %= coin;
+        }
+
+        System.out.println("count: " + count);
+    }
 }
