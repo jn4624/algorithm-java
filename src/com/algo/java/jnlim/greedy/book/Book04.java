be3e76554ead2d431c8e559250f2a6380f8337c2
LimJiNa <jn4624@gmail.com>
2023-06-21 16:04:41 +0900

    commit 2023-06-25 task

---
+++ src/com/algo/java/jnlim/greedy/book/Book04.java
@@ -0,0 +1,22 @@
+package com.algo.java.jnlim.greedy.book;
+
+import java.util.Scanner;
+
+/**
+ * 1이 될때까지
+ */
+public class Book04 {
+    public int solution(int n, int k) {
+        return 0;
+    }
+
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        int n = scan.nextInt();
+        int k = scan.nextInt();
+        scan.close();
+
+        Book04 book04 = new Book04();
+        System.out.println(book04.solution(n, k));
+    }
+}
