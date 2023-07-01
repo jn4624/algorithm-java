e4f48bf8139db7767fc15eab56b07944c1170435
GDana <danmi2857@naver.com>
2023-07-01 17:59:50 +0900

    Upload 0603

--- src/com/algo/java/hskim/greedy/book/Book04.java
+++ src/com/algo/java/hskim/greedy/book/Book04.java
@@ -1,12 +1,11 @@
 package com.algo.java.hskim.greedy.book;
 
 import java.io.*;
-import java.util.ArrayList;
 import java.util.StringTokenizer;
 
-public class Book03 {
+public class Book04 {
     /**
-     * 숫자 카드 게임(p96)
+     * 1이 될 때까지(p99)
      */
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
@@ -16,19 +15,21 @@ public class Book03 {
         int N = Integer.parseInt(st.nextToken());
         int M = Integer.parseInt(st.nextToken());
 
-        int result = Integer.MIN_VALUE;     //정수형 데이터 중 가장 작은 값으로 초기화
+        int result = 0;
 
-        // N 행 M 번 입력
-        for(int i = 0; i < N; i++) {
-            int min = Integer.MAX_VALUE;    //정수형 데이터 중 가장 큰 값으로 초기화
-            st = new StringTokenizer(br.readLine());
-
-            for(int j = 0; j < M; j++) {
-                int temp = Integer.parseInt(st.nextToken());
-                min = Math.min(min, temp);
+        while(N >= M) {
+            if(N % M != 0) {
+                N -= 1;
+                result++;
             }
 
-            result = Math.max(result, min);
+            N = N / M;
+            result++;
+        }
+
+        while(N > 1) {
+            N = N - 1;
+            result++;
         }
 
         br.close();
