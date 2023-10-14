18fc85c04ca0c717e26e84bd6a7db3ec3743bf84
GDana <danmi2857@naver.com>
2023-10-14 21:59:01 +0900

    Upload 백준 11497번: 통나무 건너뛰기

---
+++ src/com/algo/java/hskim/greedy/web/B11497.java
@@ -0,0 +1,52 @@
+package com.algo.java.hskim.greedy.web;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.PriorityQueue;
+import java.util.StringTokenizer;
+
+/**
+ * 통나무 건너뛰기
+ * https://www.acmicpc.net/problem/11497
+ */
+public class B11497 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int T = Integer.parseInt(br.readLine());
+
+        for(int i = 0; i < T; i++){
+            int N = Integer.parseInt(br.readLine());
+            StringTokenizer st = new StringTokenizer(br.readLine());
+            PriorityQueue<Integer> queue = new PriorityQueue<>((o1, o2) -> o2-o1);
+
+            int[] arr = new int[N];
+
+            for(int j = 0; j < N; j++){
+                queue.add(Integer.valueOf(st.nextToken()));
+            }
+
+            arr[N/2] = queue.poll();
+
+            int left = N/2-1;
+            int right = N/2+1;
+
+            while(!queue.isEmpty()) {
+                if(right < N){
+                    arr[right++] = queue.poll();
+                }
+                if(!queue.isEmpty()){
+                    arr[left--] = queue.poll();
+                }
+            }
+
+            int max = Math.abs(arr[0] - arr[N-1]);
+
+            for(int j = 1; j < N; j++) {
+                max = Math.max(max, Math.abs(arr[j] - arr[j-1]));
+            }
+
+            System.out.println(max);
+        }
+    }
+}
